04db7a9fcbcff5821666fe7ce720f11b89bba054
acobo2020 <a.cobo.2020@alumnos.urjc.es>
2021-12-01 23:09:59 +0100

    programa finalizado

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # P7-ReedSwitch
+
+
+## (CC-BY-NC-SA) Adrián Cobo Merino
+
+El objetivo de este esta práctica es tener la primera toma de contacto con un interruptor de lengüeta.
+
+### Funcionamiento del Programa.
+
+Este programa encenderá un led cuando el iman se separe del interruptor indicando que la puerta está abierta, y lo apagará cuando este pegado el iman al interruptor, indicando que la puerta se ha
+cerrado.
+
+```python
+    def callbackBotonPulsado1 (canal):
+    global flag1
+    if flag1 == 0:
+        pwm.ChangeDutyCycle(100)
+        print("Puerta abierta!!!!!!!!")
+        flag1 = 1
+    else:
+        pwm.ChangeDutyCycle(0)
+        print("Puerta cerrada")
+        flag1 = 0
+```
+
+**El esquema de conexión se puede intuir de**
+
+```python
+   
+   #GPIO Mode (BOARD / BCM)
+   GPIO.setmode(GPIO.BCM)
+
+   #set GPIO Pins
+   sensorPuerta = 16
+   ledRojo=21
+```
+
+Si quieres ver un video de demostracion del ej1, pulsa [aqui](https://drive.google.com/file/d/1cDLSdudxqsnL4s5az-hANzkDzSYxUYtO/view?usp=sharing).
+
+Para cualquier duda: <a.cobo.2020@alumos.urjc.es>
---
+++ interrupcionEventMejorado.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import signal
+import sys
+import RPi.GPIO as GPIO
+import time
+import threading
+
+sensorPuerta = 16
+ledRojo=21
+
+flag1 = 0
+
+
+def callbackSalir (senial, cuadro): # señal y estado cuando se produjo la interrup.
+    GPIO.cleanup () # limpieza de los recursos GPIO antes de salir
+    sys.exit(0)
+
+def callbackBotonPulsado1 (canal):
+    global flag1
+    if flag1 == 0:
+        pwm.ChangeDutyCycle(100)
+        print("Puerta abierta!!!!!!!!")
+        flag1 = 1
+    else:
+        pwm.ChangeDutyCycle(0)
+        print("Puerta cerrada")
+        flag1 = 0
+
+
+if __name__ == '__main__':
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(sensorPuerta, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    GPIO.setup (ledRojo, GPIO.OUT)
+
+    pwm = GPIO.PWM(ledRojo,100)
+    pwm.start (100)
+
+    hilo1 = threading.Thread(target=GPIO.add_event_detect(sensorPuerta, GPIO.BOTH,
+      callback=callbackBotonPulsado1, bouncetime=200))
+    hilo1.start()
+
+    hilo1.join()
+    signal.signal(signal.SIGINT, callbackSalir) # callback para CTRL+C que limpia todos los hilos anteriores
+    signal.pause() # esperamos por hilo/callback CTRL+C antes de acabar para que no se acabe solo el principal
